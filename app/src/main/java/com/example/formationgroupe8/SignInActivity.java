c5a9bdca427a6dc627a652cf7265926e34dc7dab
Mounir Ben Romdhane <mounir.benromdhane@esprit.tn>
2024-04-14 20:31:48 +0100

    done sign up UI

---
+++ app/src/main/java/com/example/formationgroupe8/SignInActivity.java
@@ -0,0 +1,19 @@
+package com.example.formationgroupe8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+
+    }
+}
